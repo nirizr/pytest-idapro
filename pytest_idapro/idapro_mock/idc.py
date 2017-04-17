54458b67023f282f2e2b8b744404429c4ef29d3d
Nir <nirizr@gmail.com>
2017-04-17 14:38:26 +0300

    Fix indentation, not everything was 4 spaces indented (#5)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- pytest_idapro/idapro_mock/idc.py
+++ pytest_idapro/idapro_mock/idc.py
@@ -1,2 +1,2 @@
 def GetIdaDirectory():
-  return "./"
+    return "./"
--- pytest_idapro/idapro_mock/mock.py
+++ pytest_idapro/idapro_mock/mock.py
@@ -1,8 +1,9 @@
 class MockObject(object):
-  def __init__(self, *args, **kwargs):
-    self.args = args
-    self.kwargs = kwargs
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
-  def __repr__(self):
-    return "<{}(mock), args: {}, kwargs: {}".format(self.__class__.__name__,
-                                                    self.args, self.kwargs)
+    def __repr__(self):
+        msg = "<{}(mock), args: {}, kwargs: {}".format(self.__class__.__name__,
+                                                       self.args, self.kwargs)
+        return msg
