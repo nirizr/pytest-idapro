9a555a0a743d8cb4ecd5c5384b739dc29cf28bec
Nir <nirizr@gmail.com>
2017-10-17 22:45:56 +0300

    Implement ida_name.get_name and bump version (#19)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- pytest_idapro/__init__.py
+++ pytest_idapro/__init__.py
@@ -1,3 +1,3 @@
 from . import plugin
 
-__version__ = '0.1.16'
+__version__ = '0.1.17'
---
+++ pytest_idapro/idapro_mock/ida_name.py
@@ -0,0 +1,3 @@
+# in IDA7, _from variable should be dropped
+def get_name(_from, ea):
+    return "dummy_function_name_{:x}".format(ea)
