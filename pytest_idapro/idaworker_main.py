adb3f7703008681cd6d91e4c5547d3ece52342d7
Nir Izraeli <nirizr@gmail.com>
2018-08-04 13:48:53 -0700

    Improve description and add extras in setup.py

--- pytest_idapro/idaworker_main.py
+++ pytest_idapro/idaworker_main.py
@@ -120,6 +120,7 @@ class IdaWorker(object):
         import plugin_worker
 
         self.pytest_config = Config.fromdictargs(option_dict, args)
+        # TODO: clean this up
         self.pytest_config.option.looponfail = False
         self.pytest_config.option.usepdb = False
         self.pytest_config.option.dist = "no"
